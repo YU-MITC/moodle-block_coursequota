9e8c8ef542c1c79b7fd4ec9be8543d27b52d1b43
YU-MITC <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
2023-02-05 02:33:49 +0900

    Update

---
+++ lang/en/block_coursequota.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file for "Course Quota block".
+ *
+ * @package   block_coursequota
+ * @copyright (C) 2023 Yamaguchi University <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author    Tomoya Saito
+ */
+
+$string['pluginname'] = 'Course Quota block';
+$string['coursequota'] = 'Course Quota';
+$string['coursequota:addinstance'] = 'Add a new Course Quota block';
+$string['mode_label'] = 'Mode';
+$string['warningonly'] = 'Warning only';
+$string['restriction'] = 'Prohibit';
+$string['notworking'] = 'Not working';
+$string['unlimited'] = 'Unlimited';
+$string['numcontents_label'] = 'Number of content';
+$string['limit_label'] = 'Limit';
+$string['privacy:metadata'] = 'The Corse Quota block only shows information about courses and does not store data itself.';
