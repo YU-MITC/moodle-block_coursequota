36f1771bc7f5cb41abb8f8f6cfb1ad22e803e513
YU-MITC <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
2023-02-05 02:34:41 +0900

    Update

---
+++ README.md
@@ -0,0 +1,58 @@
+# Course Quota block plugin for Moodle
+
+Summary
+------
+
+In Moodle course, this plugin displays number of content (resources and activities) and limit value that system administrators have applied to the course.
+This plugin also displays quota mode (Warning-only or Restriction).
+
+Requirements
+------
+
+* PHP 7.3 or greater.
+* Web browsers must support the JavaScript and HTML5.
+* "local_coursequota" plugin.
+
+Supported themes
+-----
+
+* Boost
+* Classic
+
+This plugin package might be able to work with other themes.
+
+Installation
+------
+
+Unzip this plugin, and copy the directory (blocks/coursequota) under moodle root directory (ex. /moodle).
+Installation will be completed after you log in as an administrator and access the notification menu.
+
+Targeted Moodle versions
+------
+
+Moodle 3.9, 3.10, 3.11, 4.0, 4.1
+
+Branches
+------
+
+* MOODLE_39_STABLE -> Moodle 3.9 branch
+* MOODLE_310_STABLE -> Moodle 3.10 branch
+* MOODLE_311_STABLE -> Moodle 3.11 branch
+* MOODLE_400_STABLE -> Moodle 4.0 branch
+* MOODLE_401_STABLE -> Moodle 4.1 branch
+
+First clone the repository with "git clone", then "git checkout MOODLE_400_STABLE(branch name)" to switch branches.
+
+Warning
+------
+
+* We are not responsible for any problem caused by this software. 
+* This software follows the license policy of Moodle (GNU GPL v3).
+
+Change log of Course Quota block plugin
+------
+
+Version 1.0.0
+
+* first release.
+
---
+++ lang/ja/block_coursequota.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file for "Course Quota block".
+ *
+ * @package   block_coursequota
+ * @copyright (C) 2023 Yamaguchi University <gh-cc@mlex.cc.yamaguchi-u.ac.jp>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author    Tomoya Saito
+ */
+
+$string['pluginname'] = 'コース・クオータブロック';
+$string['coursequota'] = 'コース・クオータ';
+$string['coursequota:addinstance'] = '新しいコース・クオータブロックを追加';
+$string['mode_label'] = '動作モード';
+$string['warningonly'] = '警告のみ';
+$string['restriction'] = '制限あり';
+$string['notworking'] = '停止中';
+$string['unlimited'] = '無制限';
+$string['numcontents_label'] = 'コンテンツ数';
+$string['limit_label'] = '上限';
+$string['privacy:metadata'] = 'コース・クオータブロックはコース内のコンテンツ数に関する情報を表示するのみで、いかなる個人情報も保存しません。';
